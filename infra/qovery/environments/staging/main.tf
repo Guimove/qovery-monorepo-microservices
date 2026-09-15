8c0426110c72828f533eafdd4f4aef7ef25cf8fd
Guillaume Da Silva <gdasilva@qovery.com>
2026-09-15 21:32:19 +0200

    feat(qovery): add dedicated demo environment

--- infra/qovery/environments/staging/main.tf
+++ infra/qovery/environments/staging/main.tf
@@ -1,5 +1,13 @@
-data "qovery_environment" "staging" {
-  id = var.environment_id
+resource "qovery_environment" "demo" {
+  project_id = var.project_id
+  cluster_id = var.cluster_id
+  name       = var.environment_name
+  mode       = "DEVELOPMENT"
+
+  environment_variables = [{
+    key   = "NODE_ENV"
+    value = "production"
+  }]
 }
 
 locals {
@@ -18,27 +26,27 @@ locals {
 
 # Pipeline order: databases -> migrations -> core-services -> edge
 resource "qovery_deployment_stage" "databases" {
-  environment_id = data.qovery_environment.staging.id
+  environment_id = qovery_environment.demo.id
   name           = "databases"
   description    = "Service-owned PostgreSQL databases"
 }
 
 resource "qovery_deployment_stage" "migrations" {
-  environment_id = data.qovery_environment.staging.id
+  environment_id = qovery_environment.demo.id
   name           = "migrations"
   description    = "Schema migrations run by lifecycle jobs"
   is_after       = qovery_deployment_stage.databases.id
 }
 
 resource "qovery_deployment_stage" "core_services" {
-  environment_id = data.qovery_environment.staging.id
+  environment_id = qovery_environment.demo.id
   name           = "core-services"
   description    = "Independent domain services deploy in parallel"
   is_after       = qovery_deployment_stage.migrations.id
 }
 
 resource "qovery_deployment_stage" "edge" {
-  environment_id = data.qovery_environment.staging.id
+  environment_id = qovery_environment.demo.id
   name           = "edge"
   description    = "Public gateway and web application"
   is_after       = qovery_deployment_stage.core_services.id
@@ -47,7 +55,7 @@ resource "qovery_deployment_stage" "edge" {
 module "orders" {
   source = "../../modules/service"
 
-  environment_id      = data.qovery_environment.staging.id
+  environment_id      = qovery_environment.demo.id
   deployment_stage_id = qovery_deployment_stage.core_services.id
   name                = "orders"
   repository_url      = var.repository_url
@@ -60,7 +68,7 @@ module "orders" {
 module "payments" {
   source = "../../modules/service"
 
-  environment_id      = data.qovery_environment.staging.id
+  environment_id      = qovery_environment.demo.id
   deployment_stage_id = qovery_deployment_stage.core_services.id
   name                = "payments"
   repository_url      = var.repository_url
@@ -73,7 +81,7 @@ module "payments" {
 module "api_gateway" {
   source = "../../modules/service"
 
-  environment_id      = data.qovery_environment.staging.id
+  environment_id      = qovery_environment.demo.id
   deployment_stage_id = qovery_deployment_stage.edge.id
   name                = "api-gateway"
   repository_url      = var.repository_url
@@ -87,7 +95,7 @@ module "api_gateway" {
 module "web" {
   source = "../../modules/service"
 
-  environment_id      = data.qovery_environment.staging.id
+  environment_id      = qovery_environment.demo.id
   deployment_stage_id = qovery_deployment_stage.edge.id
   name                = "web"
   repository_url      = var.repository_url
@@ -101,7 +109,7 @@ module "web" {
 module "orders_migrations" {
   source = "../../modules/lifecycle-job"
 
-  environment_id      = data.qovery_environment.staging.id
+  environment_id      = qovery_environment.demo.id
   deployment_stage_id = qovery_deployment_stage.migrations.id
   name                = "orders-migrations"
   repository_url      = var.repository_url
@@ -113,7 +121,7 @@ module "orders_migrations" {
 module "payments_migrations" {
   source = "../../modules/lifecycle-job"
 
-  environment_id      = data.qovery_environment.staging.id
+  environment_id      = qovery_environment.demo.id
   deployment_stage_id = qovery_deployment_stage.migrations.id
   name                = "payments-migrations"
   repository_url      = var.repository_url
--- infra/qovery/environments/staging/variables.tf
+++ infra/qovery/environments/staging/variables.tf
@@ -1,12 +1,23 @@
-variable "environment_id" {
-  description = "Existing Qovery staging environment UUID."
+variable "project_id" {
+  description = "Qovery project UUID that owns the demo environment."
   type        = string
+}
+
+variable "cluster_id" {
+  description = "Qovery cluster UUID where the demo environment runs."
+  type        = string
+}
+
+variable "environment_name" {
+  description = "Name of the dedicated Qovery demo environment."
+  type        = string
+  default     = "monorepo-demo"
 }
 
 variable "repository_url" {
   description = "Git URL of this monorepo."
   type        = string
-  default     = "https://github.com/acme/commerce-platform.git"
+  default     = "https://github.com/Guimove/qovery-monorepo-microservices.git"
 }
 
 variable "branch" {
